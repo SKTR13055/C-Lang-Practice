e4ef9bc212cdf46dc6b38e94f9f1f11044a9993f
Syed Khaild Tipu Razvi <110707546+SKTR13055@users.noreply.github.com>
2024-11-14 18:48:00 +0530

    Add files via upload

---
+++ line.c
@@ -0,0 +1,11 @@
+/*PROGRAM TO DISPLAY THE EQUATION OF THE LINE */
+#include<stdio.h>
+int main()
+{
+     int a,b,c;
+     a = 5;
+     b = 8;
+     c = 18;
+     printf("%dx + %dy = %d",a,b,c);
+     return 0;
+}
