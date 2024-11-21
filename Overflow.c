fe045d32317dcd5ad2b886ad7923173cac05830d
Syed Khaild Tipu Razvi <110707546+SKTR13055@users.noreply.github.com>
2024-11-21 19:09:11 +0530

    Create Understanding Overflow

---
+++ Overflow.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    int x,y;
+    short int z;
+    x = 562503;
+    y = 872391;
+    z = x+y;
+    printf("%d %d %d",x,y,z);
+  /*The output will be x=562503 y=872391 z=-6898*/
+}
